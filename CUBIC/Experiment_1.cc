4df844ade4eaa93b91e7bd22830e8b6ab5a17c58
Sahil-18 <sahilpurohit18@gmail.com>
2024-05-27 16:12:33 -0400

    Error resolution

--- CUBIC/Experiment_1.cc
+++ CUBIC/Experiment_1.cc
@@ -9,6 +9,7 @@
 #include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/netanim-module.h"
 #include "ns3/flow-monitor-module.h"
+#include "ns3/traffic-control-module.h"
 #include <iostream>
 
 using namespace ns3;
@@ -19,7 +20,7 @@ void CwndChange(Ptr<OutputStreamWrapper> stream, uint32_t oldCwnd, uint32_t newC
     *stream->GetStream() << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
 }
 
-void saveThroughput(Ptr<FlowMonitor> monitor, Ptr<IPv4FlowClassifier> classifier, Ptr<OutputStreamWrapper> stream1, Ptr<OutputStreamWrapper> stream2, Ipv4Address sender1_addr, Ipv4Address sender2_add){
+void saveThroughput(Ptr<FlowMonitor> monitor, Ptr<Ipv4FlowClassifier> classifier, Ptr<OutputStreamWrapper> stream1, Ptr<OutputStreamWrapper> stream2, Ipv4Address sender1_addr, Ipv4Address sender2_addr){
     monitor->CheckForLostPackets();
     FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
     for(std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); i++){
@@ -27,30 +28,29 @@ void saveThroughput(Ptr<FlowMonitor> monitor, Ptr<IPv4FlowClassifier> classifier
         if(t.sourceAddress == sender1_addr){
             double throughput1 = i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1024 / 1024;
             *stream1->GetStream() << Simulator::Now().GetSeconds() << "\t" << throughput1 << std::endl;
-        }
-        if(t.sourceAddress == sender2_addr){
+        }else if(t.sourceAddress == sender2_addr){
             double throughput2 = i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1024 / 1024;
             *stream2->GetStream() << Simulator::Now().GetSeconds() << "\t" << throughput2 << std::endl;
         }
     }
-    Simulator::Schedule(Seconds(1.0), &saveThroughput, monitor, classifier, throughput1, throughput2, sender1_addr, sender2_addr);
+    Simulator::Schedule(Seconds(1.0), &saveThroughput, monitor, classifier, stream1, stream2, sender1_addr, sender2_addr);
 }
 
 int main(){
     // Routers 2
-    NodeContainer routers, sender, receiver, background-traffic1, background-traffic2;;
+    NodeContainer routers, sender, receiver, background_traffic1, background_traffic2;;
     routers.Create(2);
     sender.Create(2);
     receiver.Create(2);
-    background-traffic1.Create(2);
-    background-traffic2.Create(2);
+    background_traffic1.Create(2);
+    background_traffic2.Create(2);
 
     InternetStackHelper internet;
-    routers.Install(internet);
-    sender.Install(internet);
-    receiver.Install(internet);
-    background-traffic1.Install(internet);
-    background-traffic2.Install(internet);
+    internet.Install(routers);
+    internet.Install(sender);
+    internet.Install(receiver);
+    internet.Install(background_traffic1);
+    internet.Install(background_traffic2);
 
     // Point-to-Point
     PointToPointHelper p2p, bottleneck;
@@ -63,22 +63,22 @@ int main(){
     // Network Device Containers
     NetDeviceContainer senderDevices;
     senderDevices = p2p.Install(routers.Get(0), sender.Get(0));
-    senderDevices.add(p2p.Install(routers.Get(0), sender.Get(1)));
+    senderDevices.Add(p2p.Install(routers.Get(0), sender.Get(1)));
 
     NetDeviceContainer receiverDevices;
     receiverDevices = p2p.Install(routers.Get(1), receiver.Get(0));
-    receiverDevices.add(p2p.Install(routers.Get(1), receiver.Get(1)));
+    receiverDevices.Add(p2p.Install(routers.Get(1), receiver.Get(1)));
 
-    NetDeviceContainer background-trafficDevices;
-    background-trafficDevices = p2p.Install(routers.Get(0), background-traffic1.Get(0));
-    background-trafficDevices.add(p2p.Install(routers.Get(0), background-traffic1.Get(1)));
-    background-trafficDevices.add(p2p.Install(routers.Get(1), background-traffic2.Get(0)));
-    background-trafficDevices.add(p2p.Install(routers.Get(1), background-traffic2.Get(1)));
+    NetDeviceContainer background_trafficDevices;
+    background_trafficDevices = p2p.Install(routers.Get(0), background_traffic1.Get(0));
+    background_trafficDevices.Add(p2p.Install(routers.Get(0), background_traffic1.Get(1)));
+    background_trafficDevices.Add(p2p.Install(routers.Get(1), background_traffic2.Get(0)));
+    background_trafficDevices.Add(p2p.Install(routers.Get(1), background_traffic2.Get(1)));
 
     NetDeviceContainer routerDevices;
     routerDevices = bottleneck.Install(routers.Get(0), routers.Get(1));
 
-    IPv4AddressHelper address;
+    Ipv4AddressHelper address;
     address.SetBase("10.1.1.0", "255.255.255.0");
     Ipv4InterfaceContainer senderInterfaces = address.Assign(senderDevices);
 
@@ -86,74 +86,74 @@ int main(){
     Ipv4InterfaceContainer receiverInterfaces = address.Assign(receiverDevices);
 
     address.SetBase("10.1.3.0", "255.255.255.0");
-    Ipv4InterfaceContainer background-trafficInterfaces = address.Assign(background-trafficDevices);
+    Ipv4InterfaceContainer background_trafficInterfaces = address.Assign(background_trafficDevices);
 
     address.SetBase("10.1.4.0", "255.255.255.0");
     Ipv4InterfaceContainer routerInterfaces = address.Assign(routerDevices);
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-    uint64_t bdp = 400*1024*1024*50/(8*1000);
+    uint64_t bdp = 400/8 * 1024 * 1024 /1000 * 50;
     TrafficControlHelper tch;
     tch.SetRootQueueDisc("ns3::TailDropQueueDisc", "MaxSize", QueueSizeValue(QueueSize(QueueSizeUnit::BYTES, bdp)));
     tch.install(routerDevices);
 
     uint port = 9;
     // Backgrounf Traffic from background-traffic1 to background-traffic3
-    Address sinkAddress(InetSocketAddress(background-trafficDevices.Get(5), port));
-    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddress);
-    ApplicationContainer sinkApp = sinkHelper.Install(background-traffic2.Get(0));
-    sinkApp.Start(Seconds(0.0));
-    sinkApp.Stop(Seconds(600.0));
+    Address sinkAddress1(InetSocketAddress(background_trafficDevices.Get(5), port));
+    PacketSinkHelper sinkHelper1("ns3::TcpSocketFactory", sinkAddress1);
+    ApplicationContainer sinkApp1 = sinkHelper1.Install(background_traffic2.Get(0));
+    sinkApp1.Start(Seconds(0.0));
+    sinkApp1.Stop(Seconds(600.0));
 
-    OnOffHelper onOffHelper("ns3::TcpSocketFactory", sinkAddress);
-    onOffHelper.SetAttribute("DataRate", DataRateValue(DataRate("100Kbps")));
-    onOffHelper.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer onOffApp = onOffHelper.Install(background-traffic1.Get(0));
-    onOffApp.Start(Seconds(1.0));
-    onOffApp.Stop(Seconds(600.0));
+    OnOffHelper onOffHelper1("ns3::TcpSocketFactory", sinkAddress1);
+    onOffHelper1.SetAttribute("DataRate", DataRateValue(DataRate("100Kbps")));
+    onOffHelpe1r.SetAttribute("PacketSize", UintegerValue(1024));
+    ApplicationContainer onOffApp1 = onOffHelper1.Install(background_traffic1.Get(0));
+    onOffAppq.Start(Seconds(1.0));
+    onOffAppq.Stop(Seconds(600.0));
 
     // Background Traffic from background-traffic4 to background-traffic2
-    Address sinkAddress(InetSocketAddress(background-trafficDevices.Get(3), port));
-    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddress);
-    ApplicationContainer sinkApp = sinkHelper.Install(background-traffic2.Get(1));
-    sinkApp.Start(Seconds(0.0));
-    sinkApp.Stop(Seconds(600.0));
+    Address sinkAddress2(InetSocketAddress(background_trafficDevices.Get(3), port));
+    PacketSinkHelper sinkHelper2("ns3::TcpSocketFactory", sinkAddress2);
+    ApplicationContainer sinkApp2 = sinkHelper2.Install(background_traffic2.Get(1));
+    sinkApp2.Start(Seconds(0.0));
+    sinkApp2.Stop(Seconds(600.0));
 
-    OnOffHelper onOffHelper("ns3::TcpSocketFactory", sinkAddress);
-    onOffHelper.SetAttribute("DataRate", DataRateValue(DataRate("100Kbps")));
-    onOffHelper.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer onOffApp = onOffHelper.Install(background-traffic2.Get(1));
-    onOffApp.Start(Seconds(1.0));
-    onOffApp.Stop(Seconds(600.0));
+    OnOffHelper onOffHelper2("ns3::TcpSocketFactory", sinkAddress2);
+    onOffHelper2.SetAttribute("DataRate", DataRateValue(DataRate("100Kbps")));
+    onOffHelper2.SetAttribute("PacketSize", UintegerValue(1024));
+    ApplicationContainer onOffApp2 = onOffHelper2.Install(background_traffic2.Get(1));
+    onOffApp2.Start(Seconds(1.0));
+    onOffApp2.Stop(Seconds(600.0));
 
     // Two CUBIC flows from sender to receiver one starting little later
     Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpCubic"));
-    Address sinkAddress(InetSocketAddress(receiverInterfaces.GetAddress(1), port));
-    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddress);
-    ApplicationContainer sinkApp = sinkHelper.Install(receiver.Get(0));
-    sinkApp.Start(Seconds(0.0));
-    sinkApp.Stop(Seconds(600.0));
+    Address sinkAddress3(InetSocketAddress(receiverInterfaces.GetAddress(1), port));
+    PacketSinkHelper sinkHelper3("ns3::TcpSocketFactory", sinkAddress3);
+    ApplicationContainer sinkApp3 = sinkHelper3.Install(receiver.Get(0));
+    sinkApp3.Start(Seconds(0.0));
+    sinkApp3.Stop(Seconds(600.0));
 
-    OnOffHelper onOffHelper("ns3::TcpSocketFactory", sinkAddress);
-    onOffHelper.SetAttribute("DataRate", DataRateValue(DataRate("1Gbps")));
-    onOffHelper.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer onOffApp = onOffHelper.Install(sender.Get(0));
-    onOffApp.Start(Seconds(0.0));
-    onOffApp.Stop(Seconds(600.0));
+    OnOffHelper onOffHelper3("ns3::TcpSocketFactory", sinkAddress3);
+    onOffHelper3.SetAttribute("DataRate", DataRateValue(DataRate("1Gbps")));
+    onOffHelper3.SetAttribute("PacketSize", UintegerValue(1024));
+    ApplicationContainer onOffApp3 = onOffHelper3.Install(sender.Get(0));
+    onOffApp3.Start(Seconds(0.0));
+    onOffApp3.Stop(Seconds(600.0));
 
-    Address sinkAddress(InetSocketAddress(receiverInterfaces.GetAddress(3), port));
-    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddress);
-    ApplicationContainer sinkApp = sinkHelper.Install(receiver.Get(1));
-    sinkApp.Start(Seconds(100.0));
-    sinkApp.Stop(Seconds(600.0));
+    Address sinkAddress4(InetSocketAddress(receiverInterfaces.GetAddress(3), port));
+    PacketSinkHelper sinkHelper4("ns3::TcpSocketFactory", sinkAddress4);
+    ApplicationContainer sinkApp4 = sinkHelper4.Install(receiver.Get(1));
+    sinkApp4.Start(Seconds(100.0));
+    sinkApp4.Stop(Seconds(600.0));
 
-    OnOffHelper onOffHelper("ns3::TcpSocketFactory", sinkAddress);
-    onOffHelper.SetAttribute("DataRate", DataRateValue(DataRate("1Gbps")));
-    onOffHelper.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer onOffApp = onOffHelper.Install(sender.Get(1));
-    onOffApp.Start(Seconds(100.0));
-    onOffApp.Stop(Seconds(600.0));
+    OnOffHelper onOffHelper4("ns3::TcpSocketFactory", sinkAddress4);
+    onOffHelper4.SetAttribute("DataRate", DataRateValue(DataRate("1Gbps")));
+    onOffHelper4.SetAttribute("PacketSize", UintegerValue(1024));
+    ApplicationContainer onOffApp4 = onOffHelper4.Install(sender.Get(1));
+    onOffApp4.Start(Seconds(100.0));
+    onOffApp4.Stop(Seconds(600.0));
 
     // Monitor the congestion window and throughput of both the flows continuously for 600 seconds using ascii trace files
     AsciiTraceHelper ascii;
@@ -166,21 +166,21 @@ int main(){
     Ptr<OutputStreamWrapper> stream3 = ascii.CreateFileStream("throughput1.tr");
     Ptr<OutputStreamWrapper> stream4 = ascii.CreateFileStream("throughput2.tr");
 
-    Ptr<Node> node = sender.Get(0);
-    Ptr<Ipv4> ipv4 = node->GetObject<Ipv4>();
-    Ipv4InterfaceAddress iaddr = ipv4->GetAddress(1, 0);
-    Ipv4Address sender1_addr = iaddr.GetLocal();
+    Ptr<Node> node1 = sender.Get(0);
+    Ptr<Ipv4> ipv41 = node1->GetObject<Ipv4>();
+    Ipv4InterfaceAddress iaddr1 = ipv41->GetAddress(1, 0);
+    Ipv4Address sender1_addr = iaddr1.GetLocal();
 
-    Ptr<Node> node = sender.Get(1);
-    Ptr<Ipv4> ipv4 = node->GetObject<Ipv4>();
-    Ipv4InterfaceAddress iaddr = ipv4->GetAddress(1, 0);
-    Ipv4Address sender2_addr = iaddr.GetLocal();
+    Ptr<Node> node2 = sender.Get(1);
+    Ptr<Ipv4> ipv42 = node2->GetObject<Ipv4>();
+    Ipv4InterfaceAddress iaddr2 = ipv42->GetAddress(1, 0);
+    Ipv4Address sender2_addr = iaddr2.GetLocal();
 
 
     FlowMonitorHelper flowmon;
     Ptr<FlowMonitor> monitor = flowmon.InstallAll();
-    Ptr<IPv4FlowClassifier> classifier = DynamicCast<IPv4FlowClassifier>(flowmon.GetClassifier());
-    Simulator::Schedule(Seconds(1.0), &saveThroughput, monitor, classifier, throughput1, throughput2, sender1_addr, sender2_addr);
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
+    Simulator::Schedule(Seconds(1.0), &saveThroughput, monitor, classifier, stream3, stream4, sender1_addr, sender2_addr);
     
     Simulator::Stop(Seconds(600.0));
     Simulator::Run();
